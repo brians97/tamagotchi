cb5f2c5f96e8c789a14cc98e1adb0b6cce5235a2
brians97 <isanchez.brian@gmail.com>
2020-06-22 16:36:11 -0700

    Added section classes for each attribute in html

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 // Pet Object
-class pet {
-  constructor(name, age, boredom, hunger, sleepiness) {
+class Pet {
+  constructor(name, age=0, boredom=5, hunger=5, sleepiness=5) {
     this.name = name;
-    this.age = age;
-    this.boredom = boredom;
-    this.hunger = hunger;
-    this.sleepiness = sleepiness;
+    this.age = age || 0;
+    this.boredom = boredom || 5;
+    this.hunger = hunger || 5;
+    this.sleepiness = sleepiness || 5;
   }
+
+  // Increase Age
+
+  increaseAge() {
+    this.age++;
+    if ()
+  }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
