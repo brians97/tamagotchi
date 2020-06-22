ea0472a6f817a4bdad1434504b2c3caf27734cc6
brians97 <isanchez.brian@gmail.com>
2020-06-22 14:06:05 -0700

    Pet class object is finished for now

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-
-
-
+// Pet Object
+class pet {
+  constructor(name, age, boredom, hunger, sleepiness) {
+    this.name = name;
+    this.age = age;
+    this.boredom = boredom;
+    this.hunger = hunger;
+    this.sleepiness = sleepiness;
+  }
+}
 
 
 
