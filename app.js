d826f8e8c0f25a44b7fad9875f6e7d87a4061928
brians97 <isanchez.brian@gmail.com>
2020-06-24 09:23:57 -0700

    Added button functionallity using some jQuery

--- app.js
+++ app.js
@@ -17,6 +17,33 @@ let tamagotchi = new Pet('Leopard Tech-O');
 
 
 
+$('#play-button').click(clickedPlayButton);
+$('#feed-button').click(clickedFeedButton);
+$('#sleep-button').click(clickedSleepButton);
+
+
+
+function clickedPlayButton() {
+  tamagotchi[this.boredom] = tamagotchi[this.boredom] - 1;
+  tamagotchi[this.sleepiness] = [this.sleepiness] + 2;
+  document.getElementById('boredom-scale').innerText = `${this.boredom}`;
+  console.log('it works..');
+}
+
+function clickedFeedButton() {
+  tamagotchi[this.hunger] = tamagotchi[this.hunger] - 1;
+  tamagotchi[this.sleepiness] = tamagotchi[this.sleepiness] + 1;
+  document.getElementById('hunger-scale').innerText = `${this.hunger}`;
+}
+
+function clickedSleepButton() {
+  tamagotchi[this.hunger] = tamagotchi[this.hunger] - 1;
+  tamagotchi[this.sleepiness] = tamagotchi[this.sleepiness] + 1;
+  document.getElementById('sleepiness-scale').innerText = `${this.sleepiness}`;
+}
+
+
+
 function petAttributesInHtml() {
   $('#boredom-scale').text(tamagotchi[`${this.boredom}`]);
   $('#hunger-scale').text(tamagotchi[`${this.hunger}`]);
