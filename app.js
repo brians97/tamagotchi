95c6ecc6f90af43cf1bbae0b4a9dc4ab25db0f6c
brians97 <isanchez.brian@gmail.com>
2020-06-25 11:55:24 -0700

    Updated and removed some functions that wouldn't worked and able to now update metric scale

--- app.js
+++ app.js
@@ -3,74 +3,65 @@ class Pet {
   constructor(name, age, boredom, hunger, sleepiness) {
     this.name = name;
     this.age = age;
-    this.boredom = boredom || 10;
-    this.hunger = hunger || 10;
-    this.sleepiness = sleepiness || 10;
+    this.boredom = boredom || 0;
+    this.hunger = hunger || 0;
+    this.sleepiness = sleepiness || 0;
   }
 
   // Increase Age
-
-
 }
 let tamagotchi = new Pet('Leopard Tech-O');
 
-function Loop(time = 2) {
 
-  let totalAttributes = this.boredom + this.hunger + this.sleepiness;
-
-  if (this.boredom <= 0 || this.hunger <= 0 || this.sleepiness <= 0) {
-    // let petDead = prompt(`${this.name} has died...Refresh to play again.`);
+  if (this.boredom >= 10 || this.hunger >= 10 || this.sleepiness >= 10) {
+    let petDead = prompt(`${this.name} has died...Refresh to play again.`);
   }
 
-  this.boredom = this.boredom - parseInt(time);
-  this.hunger = this.hunger - parseInt(time);
-  this.sleepiness = this.sleepiness - parseInt(time);
-
-// Event Listener
-  $('#play-button').click(playAttr);
-  $('#feed-button').click(feedAttr);
-  $('#sleep-button').click(sleepAttr);
+// // Event Listener
+//   $('#play-button').click(playAttr);
+//   $('#feed-button').click(feedAttr);
+//   $('#sleep-button').click(sleepAttr);
 
 
-  $('#boredom-scale').text(tamagotchi[`${this.boredom}`]);
-  $('#hunger-scale').text(tamagotchi[`${this.hunger}`]);
-  $('#sleepiness-scale').text(tamagotchi[`${this.sleepiness}`]);
-}
+//   $('#boredom-scale').text(tamagotchi[`${this.boredom}`]);
+//   $('#hunger-scale').text(tamagotchi[`${this.hunger}`]);
+//   $('#sleepiness-scale').text(tamagotchi[`${this.sleepiness}`]);
+// }
+
+
+let timeGame;
 
 function startGame() {
-  let timeGame = setInterval(Loop, 1000);
-  this.boredom = 10;
-  this.hunger = 10;
-  this.sleepiness = 10;
-  updateGameAttributes();
+  timeGame = setInterval(function() {
+    playAttr();
+    feedAttr();
+    sleepAttr();
+    }, 1000);
 }
-
-function updateGameAttributes() {
-  $('#boredom-scale').text(timeGame);
-}
-
 
 function playAttr() {
-  this.boredom = 10;
-  $('#boredom-scale').text(tamagotchi[`${this.boredom}`]);
-  // $('#play-button').click(playAttr);
+  tamagotchi.boredom++;
+  $('#boredom-scale').text(`${tamagotchi.boredom}`);
+  $('#play-button').click(playAttr);
+  console.log(tamagotchi);
+
 }
 
 function feedAttr() {
-  this.hunger = 10;
-  $('#hunger-scale').text(tamagotchi[`${this.hunger}`]);
-  // $('#feed-button').click(feedAttr);
+  tamagotchi.hunger++;
+  $('#hunger-scale').text(`${tamagotchi.hunger}`);
+  $('#feed-button').click(feedAttr);
 }
 
 function sleepAttr() {
-  this.sleepiness = 10;
-  $('#sleepiness-scale').text(tamagotchi[`${this.sleepiness}`]);
-  // $('#sleep-button').click(sleepAttr);
+  tamagotchi.sleepiness++;
+  $('#sleepiness-scale').text(`${tamagotchi.sleepiness}`);
+  $('#sleep-button').click(sleepAttr);
 }
 
 
-startGame();
 
+startGame();
 
 
 
